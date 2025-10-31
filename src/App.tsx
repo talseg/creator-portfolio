bd00d81d955d2196b0eefb98768462de6e2efca4
Tal Segal <talseg7@gmail.com>
2025-10-31 12:43:33 +0200

    First Porfolio commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        <a href="https://www.orsegal.net" target="_blank">
+          <h1 style={{
+            fontSize: "50px", 
+          }}>Or Segal</h1>
+        </a>
+      </h1>
+      <h1>Potfolio</h1>
+      <div className="card">
+        <button onClick={() => {}}>
+          Upload
+        </button>
+      </div>
+      <p className="read-the-docs">
+        PortPolio
+      </p>
+    </>
+  )
+}
+
+export default App
