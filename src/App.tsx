a17430e2899703b0c564f53a52488ecced12cbac
Tal Segal <talseg7@gmail.com>
2025-10-31 16:31:32 +0200

    Added Start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import './App.css'
+import { StartPage } from './components/startPage/StartPage';
 
 function App() {
-
   return (
-    <>
-      <h1>
-        <a href="https://www.orsegal.net" target="_blank">
-          <h1 style={{
-            fontSize: "50px", 
-          }}>Or Segal</h1>
-        </a>
-      </h1>
-      <h1>Potfolio</h1>
-      <div className="card">
-        <button onClick={() => {}}>
-          Upload
-        </button>
-      </div>
-      <p className="read-the-docs">
-        PortPolio
-      </p>
-    </>
+      <StartPage/>
   )
 }
 
