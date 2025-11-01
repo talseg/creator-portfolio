e6c824f16560f3b2d3693ccd01567981477c2af5
Tal Segal <talseg7@gmail.com>
2025-11-01 16:34:59 +0200

    Added import to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 import path from 'node:path';
@@ -11,7 +12,12 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  base: process.env.NODE_ENV === 'production' ? '/creator-portfolio/' : '/',
+  server: {
+    host: true,
+    allowedHosts: ['tal-pc']
+  },
   test: {
     projects: [{
       extends: true,
