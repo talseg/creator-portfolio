7010b2fa6e4723e720c449d956ffe680382915dc
Tal Segal <talseg7@gmail.com>
2025-11-15 13:38:26 +0200

    Project Table with Data

--- src/components/projectsTable/EmailPasswordDialog.stories.tsx
+++ src/components/projectsTable/EmailPasswordDialog.stories.tsx
@@ -3,9 +3,40 @@ import type { Project } from '../../database/dbInterfaces';
 import { useState } from 'react';
 import { ProjectTable } from './ProjectTable';
 
+const getName = (index: number) => {
+  switch (index) {
+    case 0:
+      return "Visual Communication Graduates Exhibition";
+      break;
+    case 2:
+      return "Sheet Magazine";
+    default:
+      return `Project Name ${index}`;
+  }
+}
+
+const createMockProjectData = (index: number): Project => {
+  return {
+    id: `project-${index}`,
+    projectName: getName(index),
+    header: "header " + index,
+    projectImageUrl: "url " + index,
+    projectIndex: index
+  }
+} 
+
+const createProjects = (numProjects: number): Project[] => {
+  const projects: Project[] = []
+  for (let index = 0; index < numProjects; index++) {
+    projects.push(createMockProjectData(index));
+  }
+  return projects;
+}
+
+
 const ProjectTableTester: React.FC = () => {
 
-  const [projects, setProjects] = useState<Project[]>([]);
+  const [projects, setProjects] = useState<Project[]>(createProjects(3));
 
   return (
     <ProjectTable projects={projects} setProjects={setProjects}/>
--- src/components/projectsTable/ProjectTable.tsx
+++ src/components/projectsTable/ProjectTable.tsx
@@ -1,3 +1,4 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import type { Project } from "../../database/dbInterfaces";
 
 export interface ProjectTableProps {
@@ -5,12 +6,32 @@ export interface ProjectTableProps {
     setProjects: (projects: Project[]) => void;
 }
 
-export const ProjectTable: React.FC<ProjectTableProps> = () => {
-
-
+export const ProjectTable: React.FC<ProjectTableProps> = ({projects}) => {
     return (
-        <> 
-            Projects Table
+        <>
+            <h2>
+                Project Table
+            </h2>
+            <TableContainer component={Paper} sx={{ maxWidth: 550 }}>
+                <Table sx={{ maxWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Project Name</TableCell>
+                            <TableCell>Project Index</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            projects.map((proj) => (
+                                <TableRow key={proj.projectIndex}>
+                                    <TableCell>{proj.projectName}</TableCell>
+                                    <TableCell>{proj.projectIndex}</TableCell>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     )
 }
