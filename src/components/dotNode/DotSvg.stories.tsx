bcd3685462cd6d805d3324247320659230345aa3
Tal Segal <talseg7@gmail.com>
2025-11-24 19:51:12 +0200

    Added styledDot

---
+++ src/components/dotNode/DotSvg.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { DotSvg, type DotSvgProps } from "./DotSvg";
+import styled from "styled-components";
+
+const DotWrapper = styled.div`
+  display: flex;
+  border: 1px solid red;
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+`
+const DotSvgTester: React.FC<DotSvgProps> = (args: DotSvgProps) => {
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+
+        <DotWrapper style={{ justifyContent: "center", alignItems: "center" }}>
+          <DotSvg {...args}></DotSvg>
+        </DotWrapper>
+
+
+        <DotWrapper style={{ alignItems: "center" }}>
+          <DotSvg {...args}></DotSvg>
+        </DotWrapper>
+
+
+        <DotWrapper style={{ justifyContent: "center" }}>
+          <DotSvg {...args}></DotSvg>
+        </DotWrapper>
+
+        <DotWrapper style={{ alignItems: "flex-end", justifyContent: "flex-end" }}>
+          <DotSvg {...args}></DotSvg>
+        </DotWrapper>
+      </div>
+    </>
+  )
+}
+
+const meta = {
+  title: 'Components/DotSvg',
+  component: DotSvgTester,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof DotSvgTester>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DotCentered: Story = {
+  args: {
+    dotSize: 26,
+  }
+};
