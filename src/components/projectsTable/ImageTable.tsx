5864b3e79960c0e312a83f7e7391477d05edab16
Tal Segal <talseg7@gmail.com>
2025-12-05 19:49:33 +0200

    Added Remove Project Image

--- src/components/projectsTable/ImageTable.tsx
+++ src/components/projectsTable/ImageTable.tsx
@@ -1,71 +1,74 @@
 import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import type { Image } from "../../database/dbInterfaces";
 import ImageSnapshot from "../imageSnapshot/ImageSnapshot";
-import { useRef } from "react";
-import { addImageToProject } from "../../database/FirebaseDb";
+import { addImageToProject, removeProjectImageFromImages } from "../../database/FirebaseDb";
+import { pickImage } from "../../utilities/pickImage";
+import { logException } from "../../utilities/exceptionUtils";
 
 interface ImageTableProps {
-    images: Image[];
-    projectId: string;
+  images: Image[];
+  projectId: string;
 }
 
 export const ImageTable: React.FC<ImageTableProps> = ({ images, projectId }) => {
 
-    const fileInputRef = useRef<HTMLInputElement | null>(null);
-    const handleAddImageClick = () => { 
+  const handleAddImageClick = () => {
+    pickImage(async (file) => {
+      if (file) {
+        try {
+          await addImageToProject(projectId, file);
+        }
+        catch (e) {
+          logException(e);
+          alert("Remove Project Image failed");
+          throw e;
+        }
+        alert("Remove Project Image success! Please refresh");
+      }
+    })
+  };
 
-        console.log("handleAddImage: causing a click in the ", )
-        // cause a click in the hidden input
-        // this will trigger the handleFileChange and get the file
-        fileInputRef.current?.click();
+  const handleRemoveImage = async (image: Image) => {
+    try {
+      await removeProjectImageFromImages(projectId, image.id);
+    }
+    catch (e) {
+      logException(e);
+      alert("remove Image failed");
+      throw e;
+    }
+    alert("Remove Project Image from images success! Please refresh");
+  }
 
-    };
-
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (!file) return;
-
-        
-        addImageToProject(projectId, file);
-        // Create a temporary URL for the selected image
-        //const url = URL.createObjectURL(file);
-        //setImageUrl(url);
-    };
-
-    return (
-        <>
-            {/* Hidden input for add image*/ }
-            <input
-                type="file"
-                accept="image/*"
-                ref={fileInputRef}
-                onChange={handleFileChange}
-                style={{ display: 'none' }}
-            />
-            <Table size="small" aria-label="images">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>DB Index</TableCell>
-                        <TableCell>Image Index</TableCell>
-                        <TableCell>
-                            <div style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
-                                <button onClick={() => handleAddImageClick()}>+</button>
-                            </div>
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {images.map((image, i) => (
-                        <TableRow key={`image-${i}`}>
-                            <TableCell component="th" scope="row">{i}</TableCell>
-                            <TableCell component="th" scope="row">{image.imageIndex}</TableCell>
-                            <TableCell>
-                                <ImageSnapshot src={image.imageUrl} alt={`image-${i}`} />
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </>
-    );
+  return (
+    <Table size="small" aria-label="images">
+      <TableHead>
+        <TableRow>
+          <TableCell>DB Index</TableCell>
+          <TableCell>Image Index</TableCell>
+          <TableCell>
+            <div style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+              <button onClick={() => handleAddImageClick()}>+</button>
+            </div>
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {images.map((image, i) => (
+          <TableRow key={`image-${i}`}>
+            <TableCell component="th" scope="row">{i}</TableCell>
+            <TableCell component="th" scope="row">{image.imageIndex}</TableCell>
+            <TableCell>
+              <ImageSnapshot
+                src={image.imageUrl}
+                alt={`image-${i}`}
+                showRemove={true}
+                onRemoveClick={() => handleRemoveImage(image)}
+              />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
