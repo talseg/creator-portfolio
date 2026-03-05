31d3e16e40196a0d0c2874f4f271cf701d12dacf
Tal Segal <talseg7@gmail.com>
2026-03-05 11:33:11 +0200

    Added ImageSwapper

---
+++ src/components/imageSwapper/ImageSwapper.stories.tsx
@@ -0,0 +1,69 @@
+import type { StoryObj } from "@storybook/react-vite";
+import { observer } from "mobx-react-lite";
+import styled from "styled-components";
+import { projectsStore } from "../../stores/projecrStore";
+import { ImageSwapper } from "./ImageSwapper";
+import { useState } from "react";
+
+const Wrapper = styled.div`
+  width: 50vw;
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+  column-gap: 120px;
+  align-items: center;
+`
+
+
+
+
+export const ImageSwapperTester: React.FC = observer(() => {
+
+  const startProj = 0;
+
+  const [projectIndex, setProjectIndex] = useState(startProj);
+  const [nextImage, setShowNext] = useState(false);
+  
+  
+
+  const projects = projectsStore.projects;
+
+  if (!projectsStore.allLoaded)
+    return <Wrapper>loading {projects.length} projects</Wrapper>;
+
+  const images = projects[projectIndex]?.images ? projects[projectIndex]?.images : [];
+  // if (images.length < 1)
+  //   return (
+  //   <Wrapper>
+  //     Loading num images: {images.length}
+  //   </Wrapper>
+  // );
+   
+
+  return (
+    <Wrapper>
+      <ImageSwapper images={images} showNext={nextImage} showNextDone={() => setShowNext(false)}/>
+      <button style={{ marginTop: "30px", width: "20%"}}
+        onClick={() => setProjectIndex(
+          (prev) => prev+ 1
+        )}>{projectIndex} Next Project</button>
+      <button style={{ marginTop: "30px", width: "20%"}}
+        onClick={() => setShowNext(true)}>Next Image</button>
+    </Wrapper>
+  );
+    
+
+});
+
+const meta = {
+  title: 'Components',
+  component: ImageSwapperTester,
+} //satisfies Meta<typeof EmailPasswordDialogTester>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ImageSwapperStory: Story = {
+  args: {
+  }
+};
---
+++ src/components/imageSwapper/ImageSwapper.tsx
@@ -0,0 +1,132 @@
+
+import type { Image } from "./../../database/dbInterfaces";
+import { useEffect, useRef, useState } from "react";
+import { useImagesPreload } from "./useImagePreload";
+import styled, { keyframes } from "styled-components";
+
+const SWAP_INTERVAL = 2500;
+
+
+// Adjust all the images height to the first image height
+const ImagesWrapper = styled.div<{ $height: number | undefined }>`
+  display: flex;
+  width: 100%;
+
+  ${({ $height }) => $height ? `height: ${$height}px;` : ""}
+  position: relative;
+  overflow: hidden;
+`;
+
+const slideIn = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0%);
+  }
+`;
+
+const SlidingImage = styled.img`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  animation: ${slideIn} 400ms ease-out forwards;
+`;
+
+const FirstImage = styled.img`
+  width: 100%;
+  height: 100%;
+`
+
+const SingleImage = styled.img`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`
+
+export interface ImageSwapperProps {
+  images: Image[];
+  showNext?: boolean;
+  showNextDone?: () => void;
+}
+
+export const ImageSwapper: React.FC<ImageSwapperProps> = ({ images, showNext, showNextDone }) => {
+
+  // Image index is the index of the currently displayed index
+  const [imageIndex, setimageIndex] = useState(0);
+  const [displayTwoImages, setDisplayTwoImages] = useState(false);
+  const [firstImageHeight, setFirstImageHeight] = useState<number | undefined>(undefined);
+
+  const shouldDecode = true;
+  const { successImages, status } = useImagesPreload(images, shouldDecode);
+  const firstImageRef = useRef<HTMLImageElement | null>(null);
+
+  // Startup
+  useEffect(() => {
+    const n = successImages.length;
+    if (n <= 1) return;
+    
+    const triggerAnimation = () => {
+      setDisplayTwoImages(true);
+      setimageIndex(prev => {
+        const next = prev + 1;
+        return next >= n ? 0 : next;
+      });
+    }
+
+    if (showNext) {
+      triggerAnimation();
+      showNextDone?.();
+    }
+      
+
+    const id = setTimeout(triggerAnimation, SWAP_INTERVAL);
+
+    return () => clearTimeout(id);
+  }, [successImages.length, imageIndex, showNext]);
+
+
+  const handleFirstImageLoad = () => {
+    if (firstImageRef.current) {
+      setFirstImageHeight(firstImageRef.current.clientHeight);
+    }
+  }
+
+  // until all images are loaded and the first image height was set - display just the first one
+  if (status !== "done" || firstImageHeight === undefined || successImages.length === 1)
+    return (
+    <ImagesWrapper $height={firstImageHeight}>
+      <FirstImage src={images[0]?.imageUrl} ref={firstImageRef} onLoad={handleFirstImageLoad} />
+    </ImagesWrapper>
+    );
+
+  let prevImageIndex: number = 0;
+  if (displayTwoImages)
+    prevImageIndex = imageIndex === 0 ? successImages.length - 1 : imageIndex - 1;
+
+  const handleAnimationEnd = () => {
+    setDisplayTwoImages(false);
+  }
+
+  return (
+    <ImagesWrapper $height={firstImageHeight}>
+      {
+        displayTwoImages ?
+          <div >
+            <SingleImage src={successImages[prevImageIndex]?.imageUrl} id={successImages[prevImageIndex]?.id} />
+            <SlidingImage
+              src={successImages[imageIndex]?.imageUrl}
+              onAnimationEnd={handleAnimationEnd}
+              id={successImages[imageIndex]?.id} />
+          </div>
+          :
+          <SingleImage src={successImages[imageIndex]?.imageUrl}
+            id={successImages[imageIndex]?.id} />
+      }
+    </ImagesWrapper>
+  );
+};
