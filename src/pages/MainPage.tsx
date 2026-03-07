e2ca8f08b23e01700e92e0865a9b102f32ad2243
Tal Segal <talseg7@gmail.com>
2026-03-07 17:45:46 +0200

    import correction

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import { DesktopPage } from "./DesktopPage";
 import { MobilePage } from "./MobilePage";
 
