a1d60c600e37ed930d36aaeb442cd3849d8010a7
Tal Segal <talseg7@gmail.com>
2026-03-22 19:59:09 +0200

    Added lazy loading of admin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
-import { AdminPage } from './pages/AdminPage';
+import { lazy, Suspense } from 'react'
 import { ProjectPage } from './pages/ProjectPage';
 import { PrevStartPage } from './pages/PrevStartPage';
 import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import { MainPage } from './pages/MainPage';
 import { MobileProjectPage } from './pages/MobileProjectPage';
 import styled from 'styled-components';
+
+const AdminPage = lazy(() =>
+  import('./pages/AdminPage').then(module => ({ default: module.AdminPage }))
+);
 
 const ErrorWrapper = styled.div`
   display: flex;
@@ -28,9 +32,13 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/prev" element={<PrevStartPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <AdminPage />
+            </Suspense>
+          } />
         <Route path="/" element={<MainPage />} />
-        
         <Route path="/project/:projectId" element={<ProjectPage />} />
         <Route path="/mobile-project/:projectId" element={<MobileProjectPage />} />
         <Route path="*" element={<NotFound />} />
